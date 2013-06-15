b6fbea818b21d48ae4cd9a5aaf3cf07d1c9fa73b
kodja <ralleks@mail.ru>
2013-06-15 11:13:01 +1000

    zada4a 3

---
+++ 3/ConsoleApplication3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int> { 44, 10, 5, 35, 5, 20 };
+            list.Reverse();
+            foreach (var value in list)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
